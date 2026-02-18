a6f4fc498e4b974a1df6116e8f9fcb61d66aa9a7
shreedharsubedi99 <shreedharsubedi99@gmail.com>
2026-02-18 18:11:08 +0545

    feat/myapp

--- lib/myProfile.dart
+++ lib/myProfile.dart
@@ -15,7 +15,70 @@ class Myprofile extends StatelessWidget {
           fontSize: 18,
           fontWeight: FontWeight.bold,
         ),
-        actions: [Icon(Icons.notification_add, color: Colors.white)],
+
+        actions: [
+          Icon(Icons.notification_add, color: Colors.white),
+          SizedBox(width: 10),
+          Icon(Icons.settings, color: Colors.white),
+          SizedBox(width: 10),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.blue,
+              backgroundImage: AssetImage('assets/images/screenshot1.png'),
+            ),
+          ),
+          SizedBox(width: 10),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              border: Border.all(color: Colors.red),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Name',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    height: 1.7,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Email',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    height: 1.7,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Address',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    height: 1.7,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
       drawer: Drawer(),
       floatingActionButton: FloatingActionButton(
